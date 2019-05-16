f1dcdee3544ec7501766513cbf4f837cd34e4c31
Charles Burch <georgia_beast89@yahoo.com>
2019-05-15 22:07:15 -0500

    cleaned up the code and setup the stucture for the react Movies setup

---
+++ src/components/movies.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+
+    render() { 
+        return (
+          <React.Fragment>
+            <h1>Hello</h1>
+          </React.Fragment>
+        );
+    }
+
+}
+ 
+export default Counter;
