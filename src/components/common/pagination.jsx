a5d6bfcc2a3c583f590e31f9a51c28a1592ce857
Charles Burch <georgia_beast89@yahoo.com>
2019-05-16 16:47:05 -0500

    created the display of the pagination ui and correct page number and installed lodash

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
-
+import _ from 'lodash';
 const Pagination = props => {
-    return null;
+    // return nav>ul.pagination>li.page-item>a.page-link;
+    //items count is the total of all the movies
+    const { itemsCount , pageSize } = props;
+    const pagesCount = Math.ceil(itemsCount / pageSize);
+    if (pagesCount === 1) return null;
+    //must include plus one because lodash leave off the last page
+    //this creates an array of the page numbers
+    const pages = _.range(1, pagesCount + 1);
+
+    return (
+      <nav>
+        <ul className="pagination">
+          {pages.map(page => (
+            <li key={page} className="page-item">
+              <a className="page-link">{page}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    );
 }
  
 export default Pagination;
