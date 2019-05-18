b01c2356f2eac570c5a54b704a41e69f12d62c29
Charles Burch <georgia_beast89@yahoo.com>
2019-05-18 11:53:07 -0500

    extracted the table into a new component to make it more resuable

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
-import TableHeader from "./common/tableHeader";
 import Like from "./common/like";
-import TableBody from './common/tableBody';
-
+import Table from "./common/table";
 
 class MoviesTable extends Component {
   columns = [
@@ -31,14 +29,12 @@ class MoviesTable extends Component {
   render() {
     const { movies,onSort, sortColumn } = this.props;
     return (
-      <table className="table">
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        />
-        <TableBody data={movies} columns={this.columns} />
-      </table>
+        <Table
+        columns={this.columns}
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
     );
   }
 }
