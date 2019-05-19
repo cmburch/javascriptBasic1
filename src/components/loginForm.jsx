f6670dbc658f21bd5af23dc8d55673bad773f518
Charles Burch <georgia_beast89@yahoo.com>
2019-05-18 23:08:41 -0500

    created a ref on the username input field just for testing purposes on the form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
- class loginForm extends Component {
+
+class loginForm extends Component {
+  username = React.createRef();
+
+//   componentDidMount() {
+//     this.username.current.focus();
+//   }
   handleSubmit = e => {
-      e.preventDefault();
-  }
-    render() {
+    e.preventDefault();
+    console.log("submitted");
+  };
+  render() {
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id='username' type="text" className="form-control" />
+            <input
+              autoFocus
+              ref={this.username}
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id='password' type="text" className="form-control" />
+            <input id="password" type="text" className="form-control" />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
