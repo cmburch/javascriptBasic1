318c222983219e46d754bc9391f970064f7fd522
Charles Burch <georgia_beast89@yahoo.com>
2019-05-19 18:27:49 -0500

    refactored code inside of input component to use rest and spread operator to reduce repeat code

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 
-const Input = ({ type,name, label, value,error, onChange }) => {
-  return (
+const Input = ({ name, label,error, ...rest }) => {
+  //...rest the rest of the properties and reduce redudant code
+    return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        value={value}
-        name={name}
-        type={type}
-        onChange={onChange}
-        id={name}
-        className="form-control"
-      />
+      <input {...rest} name={name} id={name} className="form-control" />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
