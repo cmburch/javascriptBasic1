570251d165ab66e2bfcf01efd1c51cd71f7173ce
Charles Burch <georgia_beast89@yahoo.com>
2019-05-19 14:58:09 -0500

    install joi-browser and created the initial form validation using JOI and created the JOI schema

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Joi from 'joi-browser';
 import Input from "./common/input";
 
 class loginForm extends Component {
@@ -8,7 +9,17 @@ state = {
     errors: {}
 };
 
+schema = {
+    username: Joi.string()
+      .required(),
+    password: Joi.string()
+      .required()
+  };
+
 validate = () => {
+
+    const result = Joi.validate(this.state.account, this.schema, { abortEarly: false });
+    console.log(result);
     const errors = {};
     const { account } = this.state;
     if (account.username.trim() === "")
