c0fe3abb48e10b4d19e20bf8cc86aa6721aff5c1
Charles Burch <georgia_beast89@yahoo.com>
2019-05-18 20:28:23 -0500

    added links to the table titel column and created save button that redirects back to home page

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+import { Link } from "react-router-dom";
 import Like from "./common/like";
 import Table from "./common/table";
 
 class MoviesTable extends Component {
   columns = [
-    { path: "title", label: "Title" },
+    {
+      path: "title",
+      label: "Title",
+      content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
+    },
     { path: "genre.name", label: "Genre" },
     { path: "numberInStock", label: "Stock" },
     { path: "dailyRentalRate", label: "Rate" },
@@ -27,9 +32,9 @@ class MoviesTable extends Component {
     }
   ];
   render() {
-    const { movies,onSort, sortColumn } = this.props;
+    const { movies, onSort, sortColumn } = this.props;
     return (
-        <Table
+      <Table
         columns={this.columns}
         data={movies}
         sortColumn={sortColumn}
