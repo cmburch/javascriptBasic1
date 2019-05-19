f1e5c1932a7af0bb473e65bf4597fcbd70ad2346
Charles Burch <georgia_beast89@yahoo.com>
2019-05-19 08:59:21 -0500

    refactored login form to handle multiple input dynamically

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -11,14 +11,16 @@ state = {
     console.log("submitted");
   };
 
-  handleChange = e => {
+  handleChange = ({ currentTarget: input }) => {
     //clone the state
     const account = {...this.state};
-    account.username = e.currentTarget.value;
+    account[input.name] = input.value;
+    // account.username = e.currentTarget.value;
     
     this.setState({account});
   };
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -26,16 +28,23 @@ state = {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
-              value={this.state.account.username}
+              value={account.username}
               onChange={this.handleChange}
               id="username"
+              name='username'
               type="text"
               className="form-control"
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input 
+            value={account.password}
+            onChange={this.handleChange}
+            id="password" 
+            name='password'
+            type="text" 
+            className="form-control" />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
